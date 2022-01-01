88abfcf4dd3ecc4a4eb1e807003e3dd999341e37
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-01-01 23:11:51 +1100

    added few more routes

--- main.go
+++ main.go
@@ -9,9 +9,18 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello form Snippetbox"))
 }
 
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Showing a snippet..."))
+}
+
+func createNewSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Creating a new snippet..."))
+}
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/showsnippet", showSnippet)
+	mux.HandleFunc("/createnewsnippet", createNewSnippet)
 	log.Println("Starting server on port 4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
