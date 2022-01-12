860f5ad4202e86023e1402ce5533a34065ecd3a2
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-01-12 19:58:14 +1100

    using model in create snippet handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -36,10 +36,13 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
+	app.infoLog.Println(r.Method)
+	// had to comment this because a redirect from Post handler still retains r.Method as Post
+	
+	//if r.Method != http.MethodGet {
+	//	app.clientError(w, http.StatusMethodNotAllowed)
+	//	return
+	//}
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 
 	if err != nil || id < 1 {
@@ -54,5 +57,17 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
-	w.Write([]byte("Creating new snippet..."))
+
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := "7"
+
+	id, err := app.snippets.Insert(title, content, expires)
+
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+	//w.Write([]byte("Creating new snippet..."))
 }
