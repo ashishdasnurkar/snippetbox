ebc6b80231abd1a6575b11ab75e28cb0233cacae
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-01-17 20:42:49 +1100

    added template caching

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,7 +3,6 @@ package main
 import (
 	"errors"
 	fmt "fmt"
-	"html/template"
 	"net/http"
 	"strconv"
 
@@ -22,31 +21,10 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	data := &templateData{Snippets: s}
 
-	//for _, snippet := range s {
-	//	fmt.Fprintf(w, "%v\n", snippet)
-	//}
-	files :=[]string {
-		"./ui/html/home.page.tmpl",
-		"./ui/html/base.layout.tmpl",
-		"./ui/html/footer.partial.tmpl",
-	}
-
-	ts, err := template.ParseFiles(files...)
-
-	if err != nil {
-		app.errorLog.Println(err.Error())
-		app.serverError(w,err)
-		return
-	}
-	err = ts.Execute(w, data)
-	if err != nil {
-		app.errorLog.Println(err.Error())
-		app.serverError(w, err)
-		return
-	}
-	//w.Write([]byte("Welcome to Snippetbox..."))
+	app.render(w,r, "home.page.tmpl", &templateData{
+		Snippets: s,
+	})
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
@@ -73,29 +51,9 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	data := &templateData{Snippet: s}
-
-	files := []string {
-		"./ui/html/show.page.tmpl",
-		"./ui/html/base.layout.tmpl",
-		"./ui/html/footer.partial.tmpl",
-	}
-
-	ts, err := template.ParseFiles(files...)
-
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
-
-	err = ts.Execute(w, data)
-
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
-
-	//fmt.Fprintf(w, "%v", s)
+	app.render(w,r, "show.page.tmpl", &templateData{
+		Snippet: s,
+	})
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"flag"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -15,6 +16,7 @@ type application struct {
 	errorLog *log.Logger
 	infoLog *log.Logger
 	snippets *mysql.SnippetModel
+	templateCache map[string]*template.Template
 }
 
 func main() {
@@ -36,10 +38,16 @@ func main() {
 	// before the main() function exits
 	defer db.Close()
 
+	templateCache, err := newTemplateCache("./ui/html/")
+
+	if err != nil {
+		errorLog.Fatal(err)
+	}
 	app := &application{
 		errorLog: errorLog,
 		infoLog: infoLog,
 		snippets: &mysql.SnippetModel{ DB: db},
+		templateCache: templateCache,
 	}
 
 	srv := &http.Server{
