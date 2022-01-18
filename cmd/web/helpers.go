f9109cbf7ac62fb9cc58de5bba83f9ae1725b92e
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-01-18 22:25:09 +1100

    add common data to templateData

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"runtime/debug"
+	"time"
 )
 
 func (app *application) serverError(w http.ResponseWriter, err error) {
@@ -22,6 +23,14 @@ func (app *application) notFound(w http.ResponseWriter) {
 	app.clientError(w, http.StatusNotFound)
 }
 
+func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData {
+	if td == nil {
+		td = &templateData{}
+	}
+	td.CurrentYear = time.Now().Year()
+	return td
+}
+
 func (app *application) render(w http.ResponseWriter, r *http.Request, name string, td *templateData) {
 	ts, ok := app.templateCache[name]
 
@@ -33,7 +42,7 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 	buff := new (bytes.Buffer)
 
 	// write template to a buffer to catch runtime errors
-	err := ts.Execute(buff, td)
+	err := ts.Execute(buff, app.addDefaultData(td, r))
 	if err != nil {
 		app.serverError(w, err)
 		return
