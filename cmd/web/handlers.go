bbd5daf33b2e7fc73bb258fcb24a1a9cfd08e350
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-01-27 20:36:36 +1100

    form validation refactor

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,9 +5,8 @@ import (
 	fmt "fmt"
 	"net/http"
 	"strconv"
-	"strings"
-	"unicode/utf8"
 
+	"ashishdasnurkar.com/snippetbox/pkg/forms"
 	"ashishdasnurkar.com/snippetbox/pkg/models"
 )
 
@@ -60,7 +59,9 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
-	app.render(w, r, "create.page.tmpl", nil)
+	app.render(w, r, "create.page.tmpl", &templateData{
+		Form: forms.New(nil),
+	})
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
@@ -76,35 +77,18 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	title := r.PostForm.Get("title")
-	content := r.PostForm.Get("content")
-	expires := r.PostForm.Get("expires")
-	errors := make(map[string]string)
-	if strings.TrimSpace(title) == "" {
-		errors["title"] = "This field can not be blank"
-	} else if utf8.RuneCountInString(title) > 100 {
-		errors["title"] = "This field value is too long (maximum is 100 characters)"
-	}
+	form := forms.New(r.PostForm)
 
-	if strings.TrimSpace(content) == "" {
-		errors["content"] = "This field can not be blank"
-	}
+	form.Required("title", "content", "expires")
+	form.MaxLength("title", 100)
+	form.PermittedValues("expires", "365", "7", "1")
 
-	if strings.TrimSpace(expires) == "" {
-		errors["expires"] = "This field can not be blank"
-	} else if expires != "365" && expires != "7" && expires != "1" {
-		errors["expires"] = "This field is invalid"
-	}
-
-	if len(errors) > 0 {
-		app.render(w, r, "create.page.tmpl", &templateData {
-			FormErrors: errors,
-			FormData: r.PostForm,
-		})
+	if !form.Valid() {
+		// render form again with errors
+		app.render(w, r, "create.page.tmpl", &templateData{Form: form})
 		return
 	}
-
-	id, err := app.snippets.Insert(title, content, expires)
+	id, err := app.snippets.Insert(form.Get("title"), form.Get("content"), form.Get("expires"))
 
 	if err != nil {
 		app.serverError(w, err)
