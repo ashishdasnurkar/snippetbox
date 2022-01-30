1f65789d6f7778ef6f9a7346c7f6a30ad6225b35
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-01-30 18:46:25 +1100

    added tls certificates and enabled https

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -63,7 +63,7 @@ func main() {
 		Handler: app.routes(),
 	}
 	infoLog.Printf("Starting server on %s...", *addr)
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("/tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
 }
 
