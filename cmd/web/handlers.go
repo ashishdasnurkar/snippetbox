8a63048d37efb226d919d4f208aa1aefd15b0e62
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-02-03 21:12:18 +1100

    display user signup form

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -104,7 +104,9 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) signupUserForm(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Displaying singup form...")
+	app.render(w, r, "signup.page.tmpl", &templateData{
+		Form: forms.New(nil),
+	})
 }
 
 func (app *application) signupUser(w http.ResponseWriter, r *http.Request)  {
