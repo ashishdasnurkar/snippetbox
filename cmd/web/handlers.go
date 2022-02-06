66bd71eedbc8b6f1ed13dc6e58221a5d727b0c46
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-02-06 21:27:38 +1100

    update user signup with hashed password insert

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -127,6 +127,20 @@ func (app *application) signupUser(w http.ResponseWriter, r *http.Request)  {
 		app.render(w, r, "signup.page.tmpl", &templateData{Form: form})
 		return
 	}
+
+	err = app.users.Insert(form.Get("name"), form.Get("email"), form.Get("password"))
+
+	if err != nil {
+		if errors.Is(err, models.ErrDuplicateEmail) {
+			form.Errors.Add("email", "Address already in use")
+			app.render(w, r, "signup.page.tmpl", &templateData{Form: form})
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+	app.session.Put(r, "flash", "Your signup was successful. Please Signin.")
+	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 }
 
 func(app *application) loginUserForm(w http.ResponseWriter, r *http.Request) {
