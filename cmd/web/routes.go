0c39a56e56d3b2692535d9a0539a36ded708ed57
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-02-10 19:04:32 +1100

    added user auth check on some routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -15,15 +15,15 @@ func (app *application) routes() http.Handler {
 
 	mux := pat.New()
 	mux.Get("/", dynamicMiddlewares.ThenFunc(app.home))
-	mux.Get("/snippet/create", dynamicMiddlewares.ThenFunc(app.createSnippetForm))
-	mux.Post("/snippet/create", dynamicMiddlewares.ThenFunc(app.createSnippet))
+	mux.Get("/snippet/create", dynamicMiddlewares.Append(app.requireAuthentication).ThenFunc(app.createSnippetForm))
+	mux.Post("/snippet/create", dynamicMiddlewares.Append(app.requireAuthentication).ThenFunc(app.createSnippet))
 	mux.Get("/snippet/:id", dynamicMiddlewares.ThenFunc(app.showSnippet))
 
 	mux.Get("/user/signup", dynamicMiddlewares.ThenFunc(app.signupUserForm))
 	mux.Post("/user/signup", dynamicMiddlewares.ThenFunc(app.signupUser))
 	mux.Get("/user/login", dynamicMiddlewares.ThenFunc(app.loginUserForm))
 	mux.Post("/user/login", dynamicMiddlewares.ThenFunc(app.loginUser))
-	mux.Post("/user/logout", dynamicMiddlewares.ThenFunc(app.logoutUser))
+	mux.Post("/user/logout", dynamicMiddlewares.Append(app.requireAuthentication).ThenFunc(app.logoutUser))
 
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
